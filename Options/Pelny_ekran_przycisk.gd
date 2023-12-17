05b515e7c8a72626a327861bdcccf4659697a507
tomaszpietr <149945356+tomaszpietr@users.noreply.github.com>
2023-12-17 22:50:27 +0100

    Add files via upload
    Opcje: pełny ekran, regulacja głośności muzyki

---
+++ Options/Pelny_ekran_przycisk.gd
@@ -0,0 +1,18 @@
+extends CheckButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_toggled(toggled_on):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
