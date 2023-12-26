21e1bed7885122d1e93db3fdfa7829de26463732
Taras <tarasshupikov@gmail.com>
2023-12-26 14:54:28 +0100

    Menu i opcje polaczone

---
+++ InGameMenu/InGameMenu.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_tree().change_scene_to_file("Game/GameScene.tscn")
+
---
+++ MainMenu/main_menu.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_newgame_button_pressed():
+	print("Wciśnięto przycisk \"Nowa Gra\"")
+
+
+func _on_loadgame_button_pressed():
+	print("Wciśnięto przycisk \"Wczytaj Gre\"")
+
+
+func _on_option_button_pressed():
+	get_tree().change_scene_to_file("OptionsMenu/ustawienia.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
